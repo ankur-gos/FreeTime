1a9f363441caf876257cce58674c3d09375e324a
panda707 <agoswam3@ucsc.edu>
2015-08-25 23:55:28 -0700

    Import FreeTime into Git

---
+++ FreeTime/models/User.js
@@ -0,0 +1,18 @@
+// User Object
+// Ankur Goswami
+// 8/24/2015
+
+var encryptor = require('./PasswordEncryptor.js')
+
+function FTUser(firstName, lastName, email, password){
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	encryptor.encryptPassword(password, function (err, hash){
+		if (err)
+			console.log(err)
+		else
+			this.password = hash
+	})
+}
+
