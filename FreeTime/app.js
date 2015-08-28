064888ea4b8c9b7a22c4054f2bac57cb9268aa09
panda707 <agoswam3@ucsc.edu>
2015-08-27 21:15:19 -0700

    pushDevBranch

--- FreeTime/app.js
+++ FreeTime/app.js
@@ -10,7 +10,7 @@ var users = require('./routes/users');
 var time = require('./routes/Time')
 
 var app = express();
-
+// use monk?
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
