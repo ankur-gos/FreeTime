6969d736ae4b80d7ace6a10d1ede725ca910ce5d
panda707 <agoswam3@ucsc.edu>
2015-09-08 16:08:27 -0700

    ping

--- FreeTime/public/javascripts/signup.js
+++ FreeTime/public/javascripts/signup.js
@@ -3,6 +3,7 @@ var myApp = angular.module('signup', []);
 myApp.controller('SignUpController', ['$scope', function($scope) {
 	$scope.post.signup = $scope.email
 	
+	console.log('ping');
 	$scope.postRequest = function() {
 		$http.post('http://www.myfreetime.io/api/signup', {email: $scope.post.signup}).
 		then(function(response) {
